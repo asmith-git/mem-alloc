40d3fb01140a1e13e061c47531e9738723b088ec
SolaireLibrary <solairelibrary@mail.com>
2017-01-18 12:02:58 +0000

    Added multi-threading protection to default allocators

--- src/as/mem_alloc/default_allocator.cpp
+++ src/as/mem_alloc/default_allocator.cpp
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "as/mem_alloc/default_allocator.hpp"
+#include <mutex>
 #include "as/mem_alloc/c_memory_allocator.hpp"
 #include "as/mem_alloc/advanced_upgrade_wrapper.hpp"
 
@@ -35,6 +36,52 @@ namespace as {
 
 	advanced_upgrade_wrapper<allocator_wrapper> WRAPPER;
 
+	class thread_wrapper : public advanced_memory_allocator {
+	private:
+		mutable std::mutex mLock;
+	public:
+		// Inherited from advanced_memory_allocator
+
+		size_t size_of(const void* aPtr) const throw() override {
+			mLock.lock();
+			const size_t tmp = ADV_ALLOCATOR->size_of(aPtr);
+			mLock.unlock();
+			return tmp;
+		}
+
+		size_t allocated_bytes() const throw() override {
+			mLock.lock();
+			const size_t tmp = ADV_ALLOCATOR->allocated_bytes();
+			mLock.unlock();
+			return tmp;
+		}
+
+		size_t max_allocation_size() const throw() override {
+			mLock.lock();
+			const size_t tmp = ADV_ALLOCATOR->max_allocation_size();
+			mLock.unlock();
+			return tmp;
+		}
+
+		// Inherited from memory_allocator
+
+		void* allocate(size_t aBytes) throw() override {
+			mLock.lock();
+			void* const tmp = ADV_ALLOCATOR->allocate(aBytes);
+			mLock.unlock();
+			return tmp;
+		}
+
+		bool free(void* aPtr) throw() override {
+			mLock.lock();
+			const bool tmp = ADV_ALLOCATOR->free(aPtr);
+			mLock.unlock();
+			return tmp;
+		}
+	};
+
+	thread_wrapper THREAD_WRAPPER;
+
 	void set_default_allocator(memory_allocator& aAllocator) {
 		ALLOCATOR = &aAllocator;
 		ADV_ALLOCATOR = &WRAPPER;
@@ -51,7 +98,11 @@ namespace as {
 			ONCE = false; 
 			set_default_allocator(C_ALLOCATOR);
 		}
+#ifdef ASMITH_DEFAULT_ALLOCATOR_NO_THREAD_PROTECTION
 		return *ADV_ALLOCATOR;
+#else
+		return THREAD_WRAPPER;
+#endif
 	}
 
 
